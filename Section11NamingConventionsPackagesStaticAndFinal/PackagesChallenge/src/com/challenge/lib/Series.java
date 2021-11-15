05629d11a1420996ffca121aa76389cdb2e8ee90
HarrisonWelch <harrison.r.welch@gmail.com>
2021-11-14 19:08:00 -0500

    Fixed factorial function in class Series

--- Section11NamingConventionsPackagesStaticAndFinal/PackagesChallenge/src/com/challenge/lib/Series.java
+++ Section11NamingConventionsPackagesStaticAndFinal/PackagesChallenge/src/com/challenge/lib/Series.java
@@ -14,7 +14,7 @@ public class Series {
     }
     public static int factorial(int n) {
         if (n < 0) return -1;
-        if (n == 0) return 0;
+        if (n == 0) return 1;
 
         int prod = 1;
 
