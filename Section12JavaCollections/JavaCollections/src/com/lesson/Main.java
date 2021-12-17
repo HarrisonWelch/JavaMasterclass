3d8bfd949b936030fd20aad192229e00a9ac4cb1
HarrisonWelch <harrison.r.welch@gmail.com>
2021-12-16 23:16:33 -0500

    Completed Lecture 153 Collections Overview

--- Section12JavaCollections/JavaCollections/src/com/lesson/Main.java
+++ Section12JavaCollections/JavaCollections/src/com/lesson/Main.java
@@ -3,6 +3,20 @@ package com.lesson;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // write your code here
+        Theatre theatre = new Theatre("Olympian", 8, 12);
+        theatre.getSeats();
+
+        if (theatre.reserveSeat("H11")) {
+            System.out.println("Please pay");
+        } else {
+            System.out.println("Sorry, seat is taken");
+        }
+
+        if (theatre.reserveSeat("H11")) {
+            System.out.println("Please pay");
+        } else {
+            System.out.println("Sorry, seat is taken");
+        }
     }
 }
