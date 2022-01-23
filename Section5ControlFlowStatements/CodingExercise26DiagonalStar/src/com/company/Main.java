248aca31178f515ef0c2101aa3d1c06a07f42b29
HarrisonWelch <harrison.r.welch@gmail.com>
2022-01-23 18:46:45 -0500

    Completed Coding Exercise 26 Diagonal Star

--- Section5ControlFlowStatements/CodingExercise26DiagonalStar/src/com/company/Main.java
+++ Section5ControlFlowStatements/CodingExercise26DiagonalStar/src/com/company/Main.java
@@ -5,15 +5,35 @@ public class Main {
     public static void main(String[] args) {
 	    // CodingExercise26DiagonalStar
         printSquareStar(5);
+        System.out.println("--------------------------------");
+        printSquareStar(8);
+        System.out.println("--------------------------------");
+        printSquareStar(12);
     }
 
     public static void printSquareStar(int number){
-        if (number < 5){
+        if (number < 5) {
             System.out.println("Invalid Value");
-        } else {
-            for (int row = 0; row < number; row++) {
-//                for (int row)
-            }
+            return;
         }
+        for (int i = 0; i < number; i++) {
+            System.out.print("*");
+        }
+        System.out.println("");
+        for (int row = 1; row < number-1; row++) {
+            System.out.print("*");
+            for (int col = 1; col < number-1; col++) {
+                if (row == col || number-row-1 == col) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("*");
+        }
+        for (int i = 0; i < number; i++) {
+            System.out.print("*");
+        }
+        System.out.println("");
     }
 }
